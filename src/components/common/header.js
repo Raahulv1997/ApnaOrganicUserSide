c93ba41480f46594f999ef00c441971a3d2c8fe2
shivaniwe2code <103627287+shivaniwe2code@users.noreply.github.com>
2023-01-30 10:45:52 +0530

    Cartchangee

--- src/components/common/header.js
+++ src/components/common/header.js
@@ -104,12 +104,14 @@ const Header = (props) => {
           )
           .then((response) => {
             let data = response.data;
+           
             let ProductTotal = 0;
             if (
               data.response === "cart_empty" ||
               data.response === "header error" ||
               data.error === "Please authenticate using a valid token"
             ) {
+              // console.log("data--"+data.response)
             } else {
               data.map((cdata) => {
                 ProductTotal +=
@@ -134,9 +136,10 @@ const Header = (props) => {
                     cdata.sgst) /
                     100;
               });
+              setPdata(data)
             }
  
-            setPdata(data)
+          
           });
       } catch (err) {}
     }
